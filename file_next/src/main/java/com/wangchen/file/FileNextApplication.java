6436ccb7a52d9a81d3c4c4121c4b0cdb689ce445
wangchen <1147607253@qq.com>
2020-02-04 23:24:18 +0800

    project init

---
+++ file_next/src/main/java/com/wangchen/file/FileNextApplication.java
@@ -0,0 +1,13 @@
+package com.wangchen.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileNextApplication.class, args);
+	}
+
+}
---
+++ file_next/src/test/java/com/wangchen/file/FileNextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangchen.file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileNextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
